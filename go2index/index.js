b77c8109af2f32c0bbfdb10d60ec2f46f80264ab
是小韩啊 <1655466387@qq.com>
2020-06-29 13:31:28 +0800

    Update index.js

--- go2index/index.js
+++ go2index/index.js
@@ -1,6 +1,6 @@
 // =======Options START=======
 var authConfig = {
-  siteName: "GoIndex-theme-acrou", // 网站名称
+  siteName: "韩小韩的OSS资源库", // 网站名称
   version: "1.1.1", // 程序版本
   theme: "acrou",
   // 强烈推荐使用自己的 client_id 和 client_secret
@@ -65,7 +65,7 @@ var authConfig = {
 };
 
 var themeOptions = {
-  cdn: "https://cdn.jsdelivr.net/gh/Aicirou/goindex-theme-acrou",
+  cdn: "https://cdn.jsdelivr.net/gh/uxiaohan/Han_Gindex",
   // 主题版本号
   version: "2.0.5",
   //可选默认系统语言:en/zh-chs/zh-cht
