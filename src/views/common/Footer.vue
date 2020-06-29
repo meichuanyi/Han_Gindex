028192f6b65b2038bf2c57f2a028299d87b33f80
是小韩啊 <1655466387@qq.com>
2020-06-29 13:29:32 +0800

    Add files via upload

---
+++ src/views/common/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="footer">
+    <div class="columns is-mobile is-centered">
+      <div class="field is-grouped is-grouped-multiline">
+        <div class="control">
+          <div class="tags has-addons">
+            <a
+              class="tag is-dark"
+              href="https://github.com/uxiaohan/Han_Gindex"
+            >韩小韩的GitHub</a>
+            <span class="tag is-light">
+              uxiaohan &nbsp;
+              <span class="icon">
+                <i class="fab fa-github"></i>
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {},
+  watch: {},
+  data: function () {
+    return {
+      content: ""
+    };
+  },
+  components: {},
+  methods: {}
+};
+</script>
